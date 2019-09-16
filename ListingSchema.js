d44d194f739eb5ccb20678dac64ec9c689eaf5ad
Heran Gao <herangao6@gmail.com>
2019-09-16 12:44:31 -0400

    Update ListingSchema.js

--- ListingSchema.js
+++ ListingSchema.js
@@ -34,14 +34,7 @@ listingSchema.pre('save', function(next) {
         //return;
     }
     else next();
-    /*var currentDate = new Date();
-
-    // change the updated_at field to current date
-    this.updated_at = currentDate;
-
-    // if created_at doesn't exist, add to that field
-    if (!this.created_at)
-        this.created_at = currentDate;*/
+   
 });
 
 /* Use your schema to instantiate a Mongoose model */
